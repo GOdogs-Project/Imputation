e08604c9601c745dd4e69d1b2b0ecf783be6a6e2
Anton Enright <aje@user-36-243.vpn.net.private.cam.ac.uk>
2025-06-13 20:41:49 +0100

    fixed a bug

--- New_Analysis/scripts/run_chr.sh
+++ New_Analysis/scripts/run_chr.sh
@@ -2,7 +2,7 @@
 echo $1
 echo $2
 
-plink --const-fid 0 --vcf ../broad-chr$2.downsampled.cf4.vcf.gz --out broad_plink_chr$2 --dog
+plink --const-fid 0 --vcf $1 --chr $2 --out broad_plink_chr$2 --allow-extra-chr --dog
 perl -lane 'next if($F[0] eq "0"); print $F[1]."\t".$F[0].":".$F[3];' broad_plink_chr$2.bim > broad_plink_chr$2.names
 plink --update-name broad_plink_chr$2.names --make-bed --bfile broad_plink_chr$2 --out broad_plink_chr$2.1 --dog
 perl -lane 'if(($F[4] eq "A" && $F[5] eq "T") || ($F[4] eq "T" && $F[5] eq "A") || ($F[4] eq "C" && $F[5] eq "G") || ($F[4] eq "G" && $F[5] eq "C")){ print $F[1] }' broad_plink_chr$2.1.bim > broad_plink_ambiguous.snps.chr$2
