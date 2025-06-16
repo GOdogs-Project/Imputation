f28565e77c2e8b4ff54539e4323193a0bc42e361
Prof. Anton Enright <aje39@cam.ac.uk>
2025-06-16 08:36:29 +0100

    added subfolders

--- New_Analysis/scripts/impute.pl
+++ New_Analysis/scripts/impute.pl
@@ -4,7 +4,7 @@ use POSIX;
 $impute="/mnt/research2/anton/Eleanor_Raffan/impute_v2.3.2_x86_64_static/impute2";
 $maps="/mnt/research2/anton/Eleanor_Raffan/maps";
 $dog10k="/mnt/research2/anton/Eleanor_Raffan/dog10k/panel";
-$rcmd="$impute -use_prephased_g -m $maps/chrXX.cf3.1_map.txt -h $dog10k/dog10k_plink_chrXX.phased.impute.haplotypes -l $dog10k/dog10k_plink_chrXX.phased.impute.legend -known_haps_g broad_plink_chrXX_gwas.phased.haps -int ZZ WW  -allow_large_regions -Ne 200 -o broad_plink_chrXX_gwas.phased.impute_final_chunkPP -phase";
+$rcmd="$impute -use_prephased_g -m $maps/chrXX.cf3.1_map.txt -h $dog10k/dog10k_plink_chrXX.phased.impute.haplotypes -l $dog10k/dog10k_plink_chrXX.phased.impute.legend -known_haps_g broad_plink_chrXX_gwas.phased.haps -int ZZ WW  -allow_large_regions -Ne 200 -o impute_chrXX/broad_plink_chrXX_gwas.phased.impute_final_chunkPP -phase";
 $karyotype = "/mnt/research2/anton/Eleanor_Raffan/BroadInstitute_Ostrander_genomes/Canis_lupus_familiaris.ROS_Cfam_1.0.114.karyotype.tsv";
 
 $sbatch="sbatch --partition=PlanEx2 --mem=10G --cpus-per-task 1 --ntasks 1 -o XX -e YY";
@@ -29,12 +29,17 @@ while(<FILE>){
 
 }
 
-#for ($i=1;$i<=38;$i++){
-for ($i=2;$i<=2;$i++){
+for ($i=1;$i<=38;$i++){
 	print "CHROMOSOME:$i CHRSIZE: $chr_size{$i}\n";
 	$chunks=ceil($chr_size{$i}/$chunksize);
 	print "Will divide into $chunks chunks\n";
 	$size=ceil($chr_size{$i}/$chunks);
+
+	if (-d "impute_chr$i"){
+	} else {
+		print "Making Dir: impute_chr$i\n";
+		mkdir("impute_chr$i");
+	}
 
 	print "Will divide into $chunks chunks of size $size bp\n";
 	for ($j=0;$j<$chunks;$j++){
@@ -66,4 +71,3 @@ for ($i=2;$i<=2;$i++){
 		print "\n\n";
 	}
 }
-
