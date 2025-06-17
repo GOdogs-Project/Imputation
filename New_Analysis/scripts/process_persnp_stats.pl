3565fa424d4b825124125c4f8d5ba9cdbd714c62
Anton Enright <aje@Antons-MacBook-Pro-M2.local>
2025-06-17 17:07:41 +0100

    GPL

--- New_Analysis/scripts/process_persnp_stats.pl
+++ New_Analysis/scripts/process_persnp_stats.pl
@@ -1,4 +1,16 @@
 #!/usr/bin/perl
+#
+# process_persnp_stats.pl - Anton Enright (aje39@cam.ac.uk).
+#
+# Copyright (c) 2025 - Anton Enright - University of Cambridge
+#
+# This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty 
+# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+#
 
 open(PROC,"cat impute_chr*/broad_plink_chr*_gwas.phased.impute_final_chunk*_info | grep -v \'\\-\\-\\-\' | grep -v \'\\-1\' | cut -f 1,3,6,10,11,12 -d \' \'|");
 open(FILEOUT,">all_stats_by_chr.txt");
