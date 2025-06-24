ac61a2652563850392a0c3553763a914be73bc16
Prof. Anton Enright <aje39@cam.ac.uk>
2025-06-24 11:15:37 +0100

    new script

--- New_Analysis/scripts/run_chr.pl
+++ New_Analysis/scripts/run_chr.pl
@@ -14,20 +14,32 @@
 
 $batch="sbatch --partition=PlanEx2 --mem=2G --ntasks 1 -o XXXX -e YYYY";
 
+my $runnit=0;
+
+foreach $file(@ARGV){
+        if ($file eq '--runnit'){
+                $runnit=1;
+        }
+}
+
 foreach $file (@ARGV){
-	chomp($file);
-	$stem=$file;
-	$stem=~ s/^.*chr(\d+).*$/$1/g;
-	$cmd="srun --mem=2G ./run_chr.sh $file $stem";
-	open(FILEOUT,"> /tmp/sbatch$$\_chr$stem.sh");
-	print FILEOUT "#!/bin/sh\n$cmd\n";
-	close(FILEOUT);
+        if ($file ne '--runnit'){
+        chomp($file);
+        $stem=$file;
+        $stem=~ s/^.*chr(\d+).*$/$1/g;
+        $cmd="srun --mem=2G ./run_chr.sh $file $stem";
+        open(FILEOUT,"> /tmp/sbatch$$\_chr$stem.sh");
+        print FILEOUT "#!/bin/sh\n$cmd\n";
+        close(FILEOUT);
 
-	$sbatch=$batch;
-	$sbatch=~ s/XXXX/chr$stem.out/g;
-	$sbatch=~ s/YYYY/chr$stem.err/g;
+        $sbatch=$batch;
+        $sbatch=~ s/XXXX/chr$stem.out/g;
+        $sbatch=~ s/YYYY/chr$stem.err/g;
 
-	print "$sbatch /tmp/sbatch$$\_chr$stem.sh\n";
-	system("$sbatch /tmp/sbatch$$\_chr$stem.sh");
+        print "$sbatch /tmp/sbatch$$\_chr$stem.sh\n";
+                if ($runnit){
+                system("$sbatch /tmp/sbatch$$\_chr$stem.sh");
+                }
+        }
 }
 
